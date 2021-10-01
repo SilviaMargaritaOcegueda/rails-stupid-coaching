fc28732ee1b0684247b67c7229a890a4030667bc
Silvia Ocegueda <margarita.ocegueda@gmail.com>
2021-10-01 11:00:46 -0600

    Stupid Coaching with scss

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,14 @@
-# frozen_string_literal:true
-
-# stupid coaching class
 class QuestionsController < ApplicationController
-  def ask
-    @question = params[:question]
-  end
+  def ask; end
 
   def answer
     @question = params[:question]
-
-    @answer = 'I don\'t care, get dressed and go to work!'
-    @answer = 'Silly question, get dressed and go to work!' if @question[-1] == '?'
-    @answer = 'Great!' if @question == 'I am going to work'
+    if @question =~ /i am going to work/i
+      @answer = 'Great!'
+    elsif @question.ends_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I don\'t care, get dressed and go to work!'
+    end
   end
 end
